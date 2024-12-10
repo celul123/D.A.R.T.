3127e78ebb2cd66857d7cc87009c9ab9dd57209d
celul123 <coskueylulc@gmail.com>
2024-12-10 01:21:00 +0100

    Add files via upload

--- recruitment.js
+++ recruitment.js
@@ -22,15 +22,19 @@ for(let i = 0; i < animation_elements.length; i++){
 const menuButton = document.getElementById('menu-button');
 const sidebar = document.getElementById('sidebar');
 const body = document.body;
+const overlay = document.getElementById("overlay");
 
 menuButton.addEventListener('click', function() {
+    overlay.classList.toggle("visible");
     sidebar.classList.toggle('open'); 
 });
 
 body.addEventListener('click', function(event) {
     if (sidebar.classList.contains('open') && !sidebar.contains(event.target) && !menuButton.contains(event.target)) {
+      overlay.classList.remove("visible");
       sidebar.classList.remove('open'); 
     }
+    
   });
 
  
